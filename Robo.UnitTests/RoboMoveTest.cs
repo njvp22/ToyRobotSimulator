fc6e6f5275475ff7710141330614782fcd44377d
njvp22 <priyav354@gmail.com>
2025-03-03 22:45:08 +0800

    Initial Commit

---
+++ Robo.UnitTests/RoboMoveTest.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Robo.UnitTests
+{
+    [TestClass]
+    public class RoboMoveTest
+    {
+        private StringWriter stringWriter;
+        private TextWriter originalOutput;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            // Redirect console output for verification
+            stringWriter = new StringWriter();
+            originalOutput = Console.Out;
+            Console.SetOut(stringWriter);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            Console.SetOut(originalOutput);
+            stringWriter.Dispose();
+        }
+
+        // Helper method to get console output
+        private string GetOutput()
+        {
+            return stringWriter.ToString().Trim();
+        }
+
+        [TestMethod]
+        public void TestInitialState()
+        {
+            // Arrange
+            var robo = new Robo();
+
+            // Act
+            robo.Report();
+
+            // Assert - nothing should be output since robot isn't placed
+            Assert.AreEqual("", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestPlaceValid()
+        {
+            // Arrange
+            var robo = new Robo();
+
+            // Act
+            robo.Place(1, 2, Robo.Direction.NORTH);
+            robo.Report();
+
+            // Assert
+            Assert.AreEqual("1,2,NORTH", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestPlaceInvalid()
+        {
+            // Arrange
+            var robo = new Robo();
+
+            // Act - Try to place outside the grid
+            robo.Place(5, 5, Robo.Direction.NORTH);
+            robo.Report();
+
+            // Assert - Robot should not be placed
+            Assert.AreEqual("", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestMoveValid()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(1, 1, Robo.Direction.NORTH);
+
+            // Act
+            robo.Move();
+            robo.Report();
+
+            // Assert
+            Assert.AreEqual("1,2,NORTH", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestMoveInvalid()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(0, 0, Robo.Direction.SOUTH);
+
+            // Act - Try to move off the grid
+            robo.Move();
+            robo.Report();
+
+            // Assert - Position shouldn't change
+            Assert.AreEqual("0,0,SOUTH", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestMoveWithoutPlacement()
+        {
+            // Arrange
+            var robo = new Robo();
+
+            // Act
+            robo.Move();
+            robo.Report();
+
+            // Assert - Nothing should be output
+            Assert.AreEqual("", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestLeftRotation()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(1, 1, Robo.Direction.NORTH);
+
+            // Act
+            robo.Left();
+            robo.Report();
+
+            // Assert
+            Assert.AreEqual("1,1,WEST", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestRightRotation()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(1, 1, Robo.Direction.NORTH);
+
+            // Act
+            robo.Right();
+            robo.Report();
+
+            // Assert
+            Assert.AreEqual("1,1,EAST", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestFullRotation()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(1, 1, Robo.Direction.NORTH);
+
+            // Act - Full rotation should return to original direction
+            robo.Left();
+            robo.Left();
+            robo.Left();
+            robo.Left();
+            robo.Report();
+
+            // Assert
+            Assert.AreEqual("1,1,NORTH", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestComplexMovement()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(1, 2, Robo.Direction.EAST);
+
+            // Act
+            robo.Move();
+            robo.Move();
+            robo.Left();
+            robo.Move();
+            robo.Report();
+
+            // Assert
+            Assert.AreEqual("3,3,NORTH", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestEdgeDetection()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(4, 4, Robo.Direction.NORTH);
+
+            // Act - Try to move beyond the north edge
+            robo.Move();
+            robo.Report();
+
+            // Assert - Y should remain at 4
+            Assert.AreEqual("4,4,NORTH", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestEdgeMovement()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(0, 0, Robo.Direction.EAST);
+
+            // Act - Move along the edge
+            robo.Move();
+            robo.Move();
+            robo.Left();
+            robo.Move();
+            robo.Move();
+            robo.Report();
+
+            // Assert
+            Assert.AreEqual("2,2,NORTH", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestIgnoreInvalidCommands()
+        {
+            // Arrange
+            var robo = new Robo();
+
+            // Act - Try commands before placing
+            robo.Move();
+            robo.Left();
+            robo.Right();
+            robo.Place(2, 3, Robo.Direction.WEST);
+            robo.Report();
+
+            // Assert - Only the valid commands should be processed
+            Assert.AreEqual("2,3,WEST", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestMultiplePlacements()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(1, 1, Robo.Direction.NORTH);
+
+            // Act - Replace the robot
+            robo.Move();
+            robo.Place(3, 3, Robo.Direction.EAST);
+            robo.Report();
+
+            // Assert - Position should reflect the second placement
+            Assert.AreEqual("3,3,EAST", GetOutput());
+        }
+
+        [TestMethod]
+        public void TestInvalidPlacement()
+        {
+            // Arrange
+            var robo = new Robo();
+            robo.Place(1, 1, Robo.Direction.NORTH);
+
+            // Act - Try invalid placement
+            robo.Place(-1, 6, Robo.Direction.SOUTH);
+            robo.Report();
+
+            // Assert - Should remain at original position
+            Assert.AreEqual("1,1,NORTH", GetOutput());
+        }
+    }
+
+
---
+++ RoboMove/ToyRobo.cs
@@ -0,0 +1,143 @@
+﻿
+public class Robo
+{
+    private int _x;
+    private int _y;
+    private Direction _facing;
+    private bool _placed;
+    private const int GRID_SIZE = 5;
+
+    public enum Direction
+    {
+        NORTH, SOUTH, WEST, EAST
+    }
+
+    public Robo()
+    {
+        this._x = -1;
+        this._y = -1;
+        this._facing = Direction.NORTH;
+        this._placed = false;
+    }
+
+    public void Place(int x, int y, Direction facing)
+    {
+        if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE)
+        {
+            this._x = x;
+            this._y = y;
+            this._facing = facing;
+            this._placed = true;
+        }
+
+    }
+
+    public void Move()
+    {
+        if (!_placed)
+            return;
+
+        switch (_facing)
+        {
+            case Direction.NORTH:
+                if (_y < GRID_SIZE - 1)
+                    _y++;
+                break;
+            case Direction.EAST:
+                if (_x < GRID_SIZE - 1)
+                    _x++;
+                break;
+            case Direction.WEST:
+                if (_x > 0)
+                    _x--;
+                break;
+            case Direction.SOUTH:
+                if (_y > 0)
+                    _y--;
+                break;
+        }
+    }
+    public void Left()
+    {
+        if (!_placed)
+            return;
+        _facing = TurnLeft(_facing);
+    }
+    public void Right()
+    {
+        if (!_placed)
+            return;
+        _facing = TurnRight(_facing);
+    }
+
+    public void Report()
+    {
+        if (_placed)
+            Console.WriteLine($"{_x},{_y},{_facing}");
+    }
+
+    private static Direction TurnLeft(Direction facing)
+    {
+        return facing switch
+        {
+            Direction.NORTH => Direction.WEST,
+            Direction.EAST => Direction.NORTH,
+            Direction.SOUTH => Direction.EAST,
+            Direction.WEST => Direction.SOUTH,
+            _ => throw new InvalidOperationException("Invalid Direction"),
+        };
+    }
+    private static Direction TurnRight(Direction facing)
+    {
+        return facing switch
+        {
+            Direction.NORTH => Direction.EAST,
+            Direction.EAST => Direction.SOUTH,
+            Direction.SOUTH => Direction.WEST,
+            Direction.WEST => Direction.NORTH,
+            _ => throw new InvalidOperationException("Invalid Direction"),
+        };
+    }
+
+    public static void Main(string[] args)
+    {
+        Robo robo = new Robo();
+        String input;
+        while (true)
+        {
+            input = Console.ReadLine();
+            string[] checkInput = input.Split(" ");
+            string inputUpperCase = checkInput[0].ToUpper();
+            if (inputUpperCase == "PLACE" && checkInput.Length == 2)
+            {
+                string[] position = checkInput[1].Split(",");
+                int x = int.Parse(position[0]);
+                int y = int.Parse(position[1]);
+                Direction facing = (Direction)Enum.Parse(typeof(Direction), position[2].ToUpper());
+                robo.Place(x, y, facing);
+            }
+            else if (inputUpperCase == "MOVE")
+            {
+                robo.Move();
+            }
+            else if (inputUpperCase == "LEFT")
+            {
+                robo.Left();
+            }
+            else if (inputUpperCase == "RIGHT")
+            {
+                robo.Right();
+            }
+            else if (inputUpperCase == "REPORT")
+            {
+                robo.Report();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Enter Valid Input");
+            }
+
+        }
+    }
+}
